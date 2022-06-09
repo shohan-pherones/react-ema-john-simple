5a6cf939a0db39fbed65e749c05350c3f8a7fb78
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-09 22:16:14 +0600

    subtotal added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,16 +4,25 @@ import "./Cart.css";
 const Cart = (props) => {
   const { cart } = props;
 
+  // const total = cart.reduce((previous, product) => previous + product.price, 0);
+
   let total = 0;
   for (const product of cart) {
     total = total + product.price;
   }
 
+  const shipping = (total * 10) / 100;
+  const tax = ((total + shipping) * 5) / 100;
+  const subtotal = total + shipping + tax;
+
   return (
     <div className="summery">
       <h3>Order Summery</h3>
       <p>Items Ordered: {props.cart.length}</p>
-      <p>Total: {total}</p>
+      <p>Total: ${total.toFixed(2)}</p>
+      <p>Shipping: ${shipping.toFixed(2)}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <p className="subtotal">Subtotal: ${subtotal.toFixed(2)}</p>
     </div>
   );
 };
