a93a090613b16cf9b076d07d75d83d86f82c9ee8
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-10 11:45:43 +0600

    ratings added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,9 +2,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import "./Product.css";
+import Rating from "react-rating";
 
 const Product = (props) => {
-  const { name, img, price, seller, stock } = props.product;
+  const { name, img, price, seller, stock, star } = props.product;
   const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />;
 
   return (
@@ -28,6 +29,13 @@ const Product = (props) => {
             stock!
           </small>
         </p>
+        <Rating
+          initialRating={star}
+          readonly
+          emptySymbol="fa-regular fa-star icon-color"
+          fullSymbol="fa-solid fa-star icon-color"
+        ></Rating>
+        <br />
         <button onClick={() => props.handleAddToCart(props.product)}>
           {cartIcon} Add to Cart
         </button>
